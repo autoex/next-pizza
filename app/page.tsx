ceb5bc9771f24b53727356dd831bb58e06021dab
autoex <autoex@gmail.com>
2024-10-09 09:49:56 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+     <h1>Home</h1>
+     <Button variant={"outline"}>Click</Button>
+   </div>
+
+  );
+}
