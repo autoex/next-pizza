008413d326410200b94c0acd0db1e193764e889e
autoex <autoex@gmail.com>
2024-10-14 17:23:14 +0300

    add slider for price range

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 import { Title } from './title';
 import { FilterCheckbox } from './filter-checkbox';
 import { Input } from '../ui';
+import { RangeSlider } from './range-slider';
 
 interface Props {
   className?: string;
@@ -29,10 +30,26 @@ export const Filters: React.FC<Props> = ({ className }) => {
       </div>
       <div className=' mt-5 border-y border-y-neutral-100 py-6 pb-7'>
         <p className=' font-bold mb-3'>Price from to:</p>
-        <div className="flex gap-3 mb-5">
-            <Input type='number' placeholder='0' min={0} max={200} defaultValue={0} />
-            <Input type='number' placeholder='0' min={100} max={200} defaultValue={200} />
+        <div className='flex gap-3 mb-5'>
+          <Input
+            type='number'
+            placeholder='0'
+            min={0}
+            max={200}
+            defaultValue={0}
+          />
+          <Input
+            type='number'
+            placeholder='0'
+            min={100}
+            max={200}
+            defaultValue={150}
+          />
+          
         </div>
+        
+        <RangeSlider min={0} max={200} step={10} value={[0,200]} />
+        
       </div>
     </div>
   );
