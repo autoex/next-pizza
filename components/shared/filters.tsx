d75ca749cd48af5f2b7b2f61f722a2c14bfc0309
autoex <autoex@gmail.com>
2024-10-16 12:42:38 +0300

    finished block with components list & search input

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -4,6 +4,7 @@ import { Title } from './title';
 import { FilterCheckbox } from './filter-checkbox';
 import { Input } from '../ui';
 import { RangeSlider } from './range-slider';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
 
 interface Props {
   className?: string;
@@ -45,11 +46,79 @@ export const Filters: React.FC<Props> = ({ className }) => {
             max={200}
             defaultValue={150}
           />
-          
         </div>
-        
-        <RangeSlider min={0} max={200} step={10} value={[0,200]} />
-        
+
+        <RangeSlider
+          min={0}
+          max={200}
+          step={10}
+          value={[0, 200]}
+        />
+        <CheckboxFiltersGroup
+          title='Components'
+          className='mt-5'
+          limit={6}
+          defaultItems={[
+            {
+              text: 'Cheese sauce',
+              value: '1',
+            },
+            {
+              text: 'Mozzarella',
+              value: '2',
+            },
+            {
+              text: 'Garlick',
+              value: '3',
+            },
+            {
+              text: 'Pickled cucumbers',
+              value: '4',
+            },
+            {
+              text: 'Red onion',
+              value: '5',
+            },
+            {
+              text: 'Tomato',
+              value: '6',
+            },
+          ]}
+          items={[
+            {
+              text: 'Cheese sauce',
+              value: '1',
+            },
+            {
+              text: 'Mozzarella',
+              value: '2',
+            },
+            {
+              text: 'Garlick',
+              value: '3',
+            },
+            {
+              text: 'Pickled cucumbers',
+              value: '4',
+            },
+            {
+              text: 'Red onion',
+              value: '5',
+            },
+            {
+              text: 'Tomato',
+              value: '6',
+            },
+            {
+              text: 'Tomato',
+              value: '7',
+            },
+            {
+              text: 'Tomato',
+              value: '8',
+            },
+          ]}
+        />
       </div>
     </div>
   );
