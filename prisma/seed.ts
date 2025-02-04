b9c8fbcb4c714b54d12b24bce4880a7118ddf996
autoex <autoex@gmail.com>
2025-02-04 10:49:48 +0200

    add products to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { hashSync } from 'bcrypt';
 import { prisma } from './prisma-client';
-import { categories, ingredients } from './constants';
+import { categories, ingredients, products } from './constants';
 
 async function up() {
   await prisma.user.createMany({
@@ -29,11 +29,16 @@ async function up() {
   await prisma.ingredient.createMany({
     data: ingredients
   })
+
+  await prisma.product.createMany({
+    data: products
+  })
 }
 async function down() {
   await prisma.$executeRaw `TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
   await prisma.$executeRaw `TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE;`;
   await prisma.$executeRaw `TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw `TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE;`;
 }
 async function main() {
   try {
