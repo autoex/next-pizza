57db8155077a4652ae80ba3ad87d53fd16dd86aa
autoex <autoex@gmail.com>
2025-02-04 10:45:31 +0200

    images and constants products for seed

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -139,3 +139,92 @@ export const ingredients = [
       'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738589833/ingredients/crhlnpz9jo1u1kbva5qk.png',
   },
 ].map((ingredient, index) => ({ ...ingredient, id: index + 1 }));
+
+
+export const products = [
+  {
+    name: 'Omelet with ham and mushrooms',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658024/11EE7970321044479C1D1085457A36EB_e6pz9t.webp',
+    categoryId: 2,
+  },
+  {
+    name: 'Omelet with pepperoni',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658024/11EE94ECF33B0C46BA410DEC1B1DD6F8_avggdf.webp',
+    categoryId: 2,
+  },
+  {
+    name: 'Latte Coffee',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658019/11EE7D61B0C26A3F85D97A78FEEE00AD_1_jgf07w.webp',
+    categoryId: 2,
+  },
+  {
+    name: 'Denwich with ham and cheese',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658024/11EE796FF0059B799A17F57A9E64C725_vzvpyj.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Chicken Nuggets',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658024/11EE7D618B5C7EC29350069AE9532C6E_iqptlj.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Baked Potatoes with Sauce 🌱',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658024/11EED646A9CD324C962C6BEA78124F19_wrb2cc.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Dodster',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658024/11EE796F96D11392A2F6DD73599921B9_wbonit.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Spicy Dodster 🌶️🌶️',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EE796FD3B594068F7A752DF8161D04_yr8yob.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Banana Milkshake',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EEE20B8772A72A9B60CFB20012C185_bbqgbu.webp',
+    categoryId: 4,
+  },
+  {
+    name: 'Caramel Apple Milkshake',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EE79702E2A22E693D96133906FB1B8_kodc4p.webp',
+    categoryId: 4,
+  },
+  {
+    name: 'Milkshake with Oreo Cookies',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EE796FA1F50F8F8111A399E4C1A1E3_rqdszs.webp',
+    categoryId: 4,
+  },
+  {
+    name: 'Classic Milkshake 👶',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EE796F93FB126693F96CB1D3E403FB_y6ryxp.webp',
+    categoryId: 4,
+  },
+  {
+    name: 'Irish Cappuccino',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EE7D61999EBDA59C10E216430A6093_hogmqk.webp',
+    categoryId: 5,
+  },
+  {
+    name: 'Caramel Cappuccino Coffee',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EE7D61AED6B6D4BFDAD4E58D76CF56_dk0p3m.webp',
+    categoryId: 5,
+  },
+  {
+    name: 'Coconut Latte Coffee',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EE7D61B19FA07090EE88B0ED347F42_uvazqv.webp',
+    categoryId: 5,
+  },
+  {
+    name: 'Americano Coffee',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658020/11EE7D61B044583596548A59078BBD33_hdqz9e.webp',
+    categoryId: 5,
+  },
+  {
+    name: 'Latte Coffee',
+    imageUrl: 'https://res.cloudinary.com/ddnsdfd5l/image/upload/v1738658019/11EE7D61B0C26A3F85D97A78FEEE00AD_1_jgf07w.webp',
+    categoryId: 5,
+  },
+];
